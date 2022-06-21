b18231fbc3f94602dba6f67fb9e3ed80811712c8
fitrah9ramadhan <66367366+fitrah9ramadhan@users.noreply.github.com>
2022-06-21 13:45:52 +0800

    multivar time series
    transform wb data into time series for a specific country with multi variable

--- WorldBank.py
+++ WorldBank.py
@@ -1,4 +1,3 @@
-from urllib3 import Retry
 import constant as c
 
 import pandas as pd
@@ -9,15 +8,19 @@ import seaborn as sns
 
 class WorldBankDataTransform:
     all = []
-    def __init__(self, filename: str):
+    def __init__(self, filename: dict):
         self.filename = filename
 
         # Action
         WorldBankDataTransform.all.append(self)
     
-    def specific_countries_time_series(self, country_list: list, save_file=False, filename_save=None):
+    # a variable panel data
+    def countries_panel_data(self, key_name: str , country_list: list, save_file=False, filename_save=None):
 
-        data = pd.read_csv(c.DATA_PATH+self.filename, skiprows = 3)
+        if key_name not in self.filename:
+            return print(f"{key_name} is not in filename")
+
+        data = pd.read_csv(c.DATA_PATH+self.filename[key_name], skiprows = 3)
 
         data.drop(columns = ["Country Code", "Indicator Code", "Indicator Name"], inplace = True)
         data = data[data["Country Name"].isin(country_list)]
@@ -35,9 +38,33 @@ class WorldBankDataTransform:
             print(f'your csv file has been saved in {c.SAVE_DATA_PATH} as {filename_save}')
 
         return data
-    
-    def specific_year_cross_section():
-        pass
+
+    # time series data of a country
+    def multivar_time_series(self, country: str, save_file=True, filename_save=None):
+
+        dct = self.filename
+        new_dict = {}
+        lst2 = []
+
+        for (k, v) in dct.items():
+            v = self.countries_panel_data(key_name=k, country_list=[country])
+            v = v.rename(columns={country: k})
+
+            new_dict[k] = v
+            lst2.append(k)
+            
+        df = new_dict[lst2[0]]
+        for i in range(len(lst2)):
+            if i == 0:
+                continue
+            else:
+                df = df.join(new_dict[lst2[i]])
+
+        if save_file==True and filename_save != None:
+            df.to_csv(c.SAVE_DATA_PATH+filename_save)
+            print(f'your csv file has been saved in {c.SAVE_DATA_PATH} as {filename_save}')
+
+        return df
 
     def __repr__(self):
         return f"WorldBankDataTransform('{self.filename}')"
@@ -91,6 +118,7 @@ class Visualization:
         if filename_save != None:
             fig.savefig(c.SAVE_FIG_PATH+filename_save)
             return print(f"your pic has been saved in {c.SAVE_FIG_PATH} as {filename_save}")
+
 
     def __repr__(self):
         return f"Visualization('{self.filename}')"
--- example.py
+++ example.py
@@ -1,17 +1,74 @@
-from WorldBank import WorldBankDataTransform, Visualization 
+# Example
+# Big four countries
+#####################################################
+
+# to start, uncomment and run the code below
+from worldbank import WorldBankDataTransform, Visualization 
 
 
+# in this example, we are going to get an insight from 
+# a messy multiple world bank data csv files
+# from G4 countries, those are France, Germany, Italy, 
+# and the United Kingdom
+#####################################################
+
+
+# Download csv data from data.worldbank.org/indicator
+# Get the filename and make it into variable as a string
+#####################################################
+wb_inflation_filename = 'inflation.csv'
+wb_gdp_per_capita_filename = 'gdp-per-capita-constant-US$2015.csv'
+
+
+# Create filename dictionary
+#####################################################
+filename = {'inflation': wb_inflation_filename, 'gdp':wb_gdp_per_capita_filename }
+
+
+# create an object with filename that yv got above
+#####################################################
+wb_files = WorldBankDataTransform(filename=filename)
+
+
+# as the object have created, do something that you need
+# create G4 country list
+#####################################################
 big_four_country = ['France', 'Germany', 'Italy', 'United Kingdom']
-wb_csv_filename = 'inflation-gdp-deflator-percent.csv'
 
-big_four = WorldBankDataTransform(filename=wb_csv_filename)
-ts_big_four = big_four.specific_countries_time_series(big_four_country, save_file=True, filename_save='big_four_inflation_ts.csv')
+# create panel data of inflation in G4 countries
+#####################################################
+panel_big_four_inflation = wb_files.countries_panel_data(key_name='inflation', country_list=big_four_country, save_file=True, filename_save='big_four_inflation.csv')
 
-transformed_csv = 'big_four_inflation_ts.csv'
-vis = Visualization(transformed_csv)
-vis.time_series(size=(10,8), title="Inflation in Big Four (G4) Countries", filename_save='inflation.png')
-# vis.correlation_map(size=(8,8), title="Inflation Correlation in Big Four (G4) Countries", filename_save='inflation_heatmap.png')
-# vis.boxplot(size=(20, 8), title="Big Four Inflation Boxplot", filename_save='inflation_boxplot.png')
 
-print(WorldBankDataTransform.all)
-print(Visualization.all)
+# create panel data of gdp per capita in G4 countries
+#####################################################
+panel_big_four_gdp_per_capita = wb_files.countries_panel_data(key_name='gdp', country_list=big_four_country, save_file=True, filename_save='big_four_gdp_per_capita.csv')
+
+
+# create time series data for a countries with multivariable
+# for example: France
+#####################################################
+ts_inflation_gdp_france = wb_files.multivar_time_series('France', save_file=True, filename_save='france_inflation_and_gdp_ts.csv')
+
+
+
+# You also can visualize your data that has created from the object above, 
+# that is inflation time series data of G4 (France, Germany, Italy, and The United Kingdom)
+# there should be csv file generated and the output show you where is the csv file located
+# get filename created from above's as a variable
+#####################################################
+transformed_csv = 'big_four_inflation.csv'
+
+
+# create a visualization object with its csv files
+#####################################################
+vis = Visualization(filename=transformed_csv)
+
+
+# after the below code run, there should be a png file generated, 
+# and the output would show you where is the png file located
+#####################################################
+vis.time_series(size=(10,8), title="Inflation in Big Four (G4) Countries", filename_save='inflation_ts_plot.png')
+vis.correlation_map(size=(8,8), title="Inflation Correlation in Big Four (G4) Countries", filename_save='inflation_heatmap.png')
+vis.boxplot(size=(20, 8), title="Big Four Inflation Boxplot", filename_save='inflation_boxplot.png')
+
